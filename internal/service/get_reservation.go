022dfb5b8268df96c3b4c7846bcb007579b3c516
Carlos Queiroz <558654+caxqueiroz@users.noreply.github.com>
2026-05-22 12:15:59 +0800

    feat(service): implement GetReservation; drop reservation stubs

---
+++ internal/service/get_reservation.go
@@ -0,0 +1,18 @@
+// internal/service/get_reservation.go
+package service
+
+import (
+	"context"
+
+	"connectrpc.com/connect"
+
+	ledgerv1 "github.com/caxqueiroz/doubleledger/gen/proto/ledger/v1"
+)
+
+func (s *Server) GetReservation(ctx context.Context, req *connect.Request[ledgerv1.GetReservationRequest]) (*connect.Response[ledgerv1.GetReservationResponse], error) {
+	res, err := s.Store.GetReservation(ctx, req.Msg.GetTenantId(), req.Msg.GetReservationId())
+	if err != nil {
+		return nil, ToConnectError(err)
+	}
+	return connect.NewResponse(&ledgerv1.GetReservationResponse{Reservation: reservationToProto(res)}), nil
+}
